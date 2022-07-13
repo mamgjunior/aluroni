d8635af6266ba9161546ce28819826199508f172
marcos_maia <marcos.gomes@underprotection.com.br>
2022-07-13 15:35:44 -0300

    first commit

---
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -0,0 +1,44 @@
+import filtros from './filtros.json';
+import styles from './Filtros.module.scss';
+import classNames from 'classnames';
+
+interface IOpcao {
+  id: number;
+  label: string;
+}
+
+interface IFiltro {
+  filtro: number | null;
+  setFiltro: React.Dispatch<React.SetStateAction<number | null>>;
+}
+
+export default function Filtros({ 
+  filtro, 
+  setFiltro
+}: IFiltro){
+  function selecionarFiltro(opcao: IOpcao) {
+    if(filtro === opcao.id)
+      return setFiltro(null);
+    
+    return setFiltro(opcao.id);
+  }
+
+  return (
+    <div 
+      className={styles.filtros}
+    >
+      {filtros.map((opcao) => (
+        <button 
+          className={classNames({
+            [styles.filtros__filtro]: true,
+            [styles['filtros__filtro--ativo']]: filtro === opcao.id
+          })}
+          key={opcao.id}
+          onClick={() => selecionarFiltro(opcao)}
+        >
+          { opcao.label }
+        </button>
+      ))}
+    </div>
+  );
+}
---
+++ src/pages/Cardapio/Itens/index.tsx
@@ -0,0 +1,66 @@
+import styles from './itens.module.scss';
+import cardapio from './itens.json';
+import Item from './Item';
+import { useEffect, useState } from 'react';
+
+interface IProps {
+  busca: string;
+  filtro: number | null;
+  ordenador: string;
+}
+
+export default function Itens(props: IProps){
+  const [ lista, setlista ] = useState(cardapio);  
+  const { busca, filtro, ordenador } = props;
+
+  function testaBusca(title: string) {
+    const regex = new RegExp(busca, 'i');
+    return regex.test(title);
+  }
+
+  function testaFiltro(id: number) {
+    if(filtro !== null)
+      return filtro === id;
+    return true;
+  }
+
+  function ordenar(novalista: typeof cardapio) {
+    switch (ordenador) {
+      case 'porcao':
+        return ordenarPropriedadeCrescente(novalista, 'size');
+      case 'qtd_pessoas':
+        return ordenarPropriedadeCrescente(novalista, 'serving');
+      case 'preco':
+        return ordenarPropriedadeCrescente(novalista, 'price');
+      default:
+        return novalista;
+    }
+  }
+
+  const ordenarPropriedadeCrescente = (
+    lista: typeof cardapio,
+    propriedade: 'size' | 'serving' | 'price'
+  ) => {
+    return lista.sort((a, b) => (a[propriedade] > b[propriedade] ? 1 : -1));
+  };
+
+  useEffect(() => {
+    const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
+    setlista(ordenar(novaLista));
+  }, [busca, filtro, ordenador]);
+  
+  return(
+    <div
+      className={styles.itens}
+    >
+      {
+        lista.map((item) => (
+          <Item 
+            key={item.id}
+            { ...item }
+          />          
+        ))
+      }
+    </div>
+  );
+}
